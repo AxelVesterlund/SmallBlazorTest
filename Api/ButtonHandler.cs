03c4c1b62c80cbc4e714632cb68b9df89308349f
Axel Vesterlund <59090010+AxelVesterlund@users.noreply.github.com>
2022-03-31 09:54:13 +0200

    commit

---
+++ Api/ButtonHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+using BlazorApp.Shared;
+
+namespace BlazorApp.Api
+{
+    public static class ButtonHandler
+    {
+        [FunctionName("ButtonHandler")]
+        public static IActionResult Run(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+    ILogger log)
+        {
+            return new OkObjectResult(0);
+        }
+    }
+}
---
+++ Client/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+internal class Person
+{
+    public string firstname { get; set; }
+    public int age { get; set; }
+    [JsonProperty(PropertyName = "id")]
+    public string id { get; set; }
+    public string _rid { get; set; }
+    public string _self { get; set; }
+    public string _etag { get; set; }
+    public string _attachments { get; set; }
+    public int _ts { get; set; }
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+
+    public Person(string firstname, int age, string id = "")
+    {
+        this.firstname = firstname;
+        this.age = age;
+
+        if(!string.IsNullOrEmpty(id))
+            this.id = id;
+        else
+            this.id = Guid.NewGuid().ToString();
+    }
+}
